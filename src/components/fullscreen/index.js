7ce315ffcd84a03a46b8fe502597a64b8afc8c1e
A.Alekseev <angelpsymail@gmail.com>
2017-09-16 03:15:07 +0500

    markup fullscreen menu

--- src/components/fullscreen/index.js
+++ src/components/fullscreen/index.js
@@ -1,3 +1,26 @@
 import './index.scss';
 
 import '../menu-fullscreen/index';
+
+const CLASSES = {
+    containerActive: 'b-fullscreen--active',
+};
+
+/**
+ * @type {Element}
+ */
+const container = document.querySelector('.b-fullscreen');
+
+/**
+ * @type {Element}
+ */
+const btn = container.querySelector('.js-togle-menu-fullscreen');
+
+btn.addEventListener('click', toggleMenu);
+
+/**
+ * @param {Event} event
+ */
+function toggleMenu(event) {
+    container.classList.toggle(CLASSES.containerActive);
+}
