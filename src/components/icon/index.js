0cb4d6c1a44676432d7c5c8c8b89a9aa2ab0b4bb
A.Alekseev <angelpsymail@gmail.com>
2017-09-17 03:23:06 +0500

    markup contacts

--- src/components/icon/index.js
+++ src/components/icon/index.js
@@ -4,6 +4,10 @@ import githubSvg from './assets/github.svg';
 import inSvg from './assets/in.svg';
 import arrowDownSvg from './assets/arrow_down.svg';
 import aboutHeaderSvg from './assets/about_header.svg';
+import skypeSvg from './assets/skype.svg';
+import envelopeSvg from './assets/envelope.svg';
+import phoneSvg from './assets/phone.svg';
+import mapMarkerSvg from './assets/map_marker.svg';
 
 import {replacementViewbox} from 'src/common/scripts/helpers/svg';
 
@@ -27,6 +31,22 @@ import {replacementViewbox} from 'src/common/scripts/helpers/svg';
     {
         id: 'about_header',
         symbol: aboutHeaderSvg,
+    },
+    {
+        id: 'skype',
+        symbol: skypeSvg,
+    },
+    {
+        id: 'envelope',
+        symbol: envelopeSvg,
+    },
+    {
+        id: 'phone',
+        symbol: phoneSvg,
+    },
+    {
+        id: 'map_marker',
+        symbol: mapMarkerSvg,
     },
 ].forEach((item) => {
     replacementViewbox(item.id, item.symbol);
