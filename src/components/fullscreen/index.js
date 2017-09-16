a097c323e0ebdd169a767582112b3ffe6454c86c
A.Alekseev <angelpsymail@gmail.com>
2017-09-16 20:36:43 +0500

    added tabindex for item of fillscreen menu

--- src/components/fullscreen/index.js
+++ src/components/fullscreen/index.js
@@ -16,11 +16,43 @@ const container = document.querySelector('.b-fullscreen');
  */
 const btn = container.querySelector('.js-togle-menu-fullscreen');
 
-btn.addEventListener('click', toggleMenu);
+/**
+ * @type {Element[]}
+ */
+const links = Array.prototype.slice.call(container.querySelectorAll('a'));
 
+/**
+ * @type {boolean}
+ */
+let menuIsOpen = container.classList.contains(CLASSES.containerActive);
+
+
+/**
+ * @param {Boolean} menuIsOpen
+ * @param {Element[]} elements
+ */
+function toggleTabindexLinks(menuIsOpen, elements) {
+    const tabindex = menuIsOpen ? 100 : -1;
+    elements.forEach((link) => {
+        link.setAttribute('tabindex', tabindex);
+    });
+}
+
+// TODO: реализовать зацикливание перехода по tab при открытии меню
+// TODO: реализовать закрытие по esc
 /**
  * @param {Event} event
  */
 function toggleMenu(event) {
     container.classList.toggle(CLASSES.containerActive);
+    menuIsOpen = !menuIsOpen;
+    toggleTabindexLinks(menuIsOpen, links);
 }
+
+/**
+ *
+ */
+(function init() {
+    btn.addEventListener('click', toggleMenu);
+    toggleTabindexLinks(menuIsOpen, links);
+})();
