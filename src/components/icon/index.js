36b5e1af1784d20b1c7014af17d00b7acd171fa9
A.Alekseev <angelpsymail@gmail.com>
2017-09-17 22:21:48 +0500

    base layout the page of works and markup the components works-main and works-slider

--- src/components/icon/index.js
+++ src/components/icon/index.js
@@ -4,10 +4,14 @@ import githubSvg from './assets/github.svg';
 import inSvg from './assets/in.svg';
 import arrowDownSvg from './assets/arrow_down.svg';
 import aboutHeaderSvg from './assets/about_header.svg';
+import worksHeaderSvg from './assets/works_header.svg';
 import skypeSvg from './assets/skype.svg';
 import envelopeSvg from './assets/envelope.svg';
 import phoneSvg from './assets/phone.svg';
 import mapMarkerSvg from './assets/map_marker.svg';
+import linkSvg from './assets/link.svg';
+import portfArrowDownSvg from './assets/portf_arrow_down.svg';
+import portfArrowUpSvg from './assets/portf_arrow_up.svg';
 
 import {replacementViewbox} from 'src/common/scripts/helpers/svg';
 
@@ -33,6 +37,10 @@ import {replacementViewbox} from 'src/common/scripts/helpers/svg';
         symbol: aboutHeaderSvg,
     },
     {
+        id: 'works_header',
+        symbol: worksHeaderSvg,
+    },
+    {
         id: 'skype',
         symbol: skypeSvg,
     },
@@ -47,6 +55,18 @@ import {replacementViewbox} from 'src/common/scripts/helpers/svg';
     {
         id: 'map_marker',
         symbol: mapMarkerSvg,
+    },
+    {
+        id: 'link',
+        symbol: linkSvg,
+    },
+    {
+        id: 'portf_arrow_down',
+        symbol: portfArrowDownSvg,
+    },
+    {
+        id: 'portf_arrow_up',
+        symbol: portfArrowUpSvg,
     },
 ].forEach((item) => {
     replacementViewbox(item.id, item.symbol);
--- src/pages/works/index.js
+++ src/pages/works/index.js
@@ -1,1 +1,7 @@
 import './index.scss';
+
+import '../../components/header/index';
+import '../../components/hero/index';
+import '../../components/_works/works-main/index';
+import '../../components/_works/works-what/index';
+import '../../components/footer/index';
