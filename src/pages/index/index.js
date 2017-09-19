6906f928697f2466aa8688ef06ce6c4a14c66580
A.Alekseev <angelpsymail@gmail.com>
2017-09-19 20:27:18 +0500

    rename component login card to login form

--- src/pages/index/index.js
+++ src/pages/index/index.js
@@ -3,5 +3,5 @@ import './index.scss';
 
 import '../../components/_index/index-header/index';
 import '../../components/_index/index-card/index';
-import '../../components/login-card/index';
+import '../../components/login-form/index';
 import '../../components/_index/index-footer/index';
