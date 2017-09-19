47725bd3ed04032e63e7c34f454c90d0818c513f
A.Alekseev <angelpsymail@gmail.com>
2017-09-19 23:01:42 +0500

    added page login

--- src/pages/index/index.js
+++ src/pages/index/index.js
@@ -4,4 +4,4 @@ import './index.scss';
 import '../../components/_index/index-header/index';
 import '../../components/_index/index-card/index';
 import '../../components/login-form/index';
-import '../../components/_index/index-footer/index';
+import '../../components/simple-footer/index';
