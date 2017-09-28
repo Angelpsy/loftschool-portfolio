66af5f0ad8d0d4bb612b86c11c2e4b3b1d60d632
A.Alekseev <angelpsymail@gmail.com>
2017-09-29 02:16:47 +0500

    added blur effect on the callback form on page of works

--- src/components/_works/works-form/index.js
+++ src/components/_works/works-form/index.js
@@ -1,3 +1,51 @@
 import './index.scss';
 
 import '../../title/index';
+
+const CLASSES = {
+    blurEl: 'b-works-form__blur',
+};
+
+const container = document.querySelector('.b-works-form');
+
+const sectionEl = document.querySelector('.b-works-what');
+
+const blurEl = document.createElement('div');
+
+blurEl.classList.add(CLASSES.blurEl);
+
+/**
+ *
+ */
+function drawBlur() {
+    const w = sectionEl.offsetWidth;
+    const h = sectionEl.offsetHeight;
+    const l = container.offsetLeft;
+    // const t = container.offsetTop;
+    const b = h - (container.offsetTop + container.offsetHeight);
+
+    blurEl.style.width = w + 'px';
+    blurEl.style.height = h + 'px';
+    blurEl.style.left = `-${l}px`;
+    blurEl.style.bottom = `-${b}px`;
+
+    console.log(b);
+}
+
+/**
+ *
+ */
+function init() {
+    container.appendChild(blurEl);
+    drawBlur();
+
+    // setTimeout(drawBlur, 1000);
+
+    window.addEventListener('resize', drawBlur);
+
+    Array.prototype.slice.call(container.querySelectorAll('textarea')).forEach((el) => {
+        el.addEventListener('resize', drawBlur);
+    });
+}
+
+window.addEventListener('load', init);
